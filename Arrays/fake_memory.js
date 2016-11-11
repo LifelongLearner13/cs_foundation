10ce24571c2fbd0c5ee604fba9212eda195546b7
LifelongLearner13 <sierragregg@gmail.com>
2016-11-11 16:09:44 -0600

    Added js array implementation and more notes

--- Arrays/fake_memory.js
+++ Arrays/fake_memory.js
@@ -4,53 +4,51 @@
  * Source: https://gist.github.com/oampo
  */
 
-// Float64Array(1024) defines an array of 64-bit
-// floating point numbers (corresponding to the
-// C double data type). Indexes 0-1024 are assigned
-// a value of 0.
-var memory = new Float64Array(1024);
-var head = 0;
+let Memory = function() {
 
-var allocate = function(size) {
-    if (head + size > memory.length) {
-        return null;
-    }
-    var start = head;
-    head += size;
-    return start;
+  // Float64Array(1024) defines an array of 64-bit
+  // floating point numbers (corresponding to the
+  // C double data type). Indexes 0-1024 are assigned
+  // a value of 0.
+  this.slots = new Float64Array(1024);
+  this.head = 0;
 };
 
-var free = function(ptr) {
+Memory.prototype.allocate = function(size) {
+  if (head + size > this.slots.length) {
+    return null; // Not enough memory :(
+  }
+  let start = head;
+  head += size;
+  return start;
 };
 
-var copy = function(to, from, size) {
+Memory.prototype.free = function(ptr) {
+  // Perfict example of why this is a bad representation of
+  // computer memory
+};
+
+Memory.prototype.copy = function(to, from, size) {
     if (from === to) {
         return;
-    }
-    else if (from > to) {
-        // Iterate forwards
+    } else if (from > to) {
         for (var i=0; i<size; i++) {
-            set(to + i, get(from + i));
+            this.set(to + i, this.get(from + i));
         }
     }
     else {
-        // Iterate backwards
         for (var i=size - 1; i>=0; i--) {
-            set(to + i, get(from + i));
+            this.set(to + i, this.get(from + i));
         }
     }
 };
 
-var get = function(ptr) {
+Memory.prototype.get = function(ptr) {
     return memory[ptr];
 };
 
-var set = function(ptr, value) {
+Memory.prototype.set = function(ptr, value) {
     memory[ptr] = value;
 };
 
-exports.allocate = allocate;
-exports.free = free;
-exports.copy = copy;
-exports.get = get;
-exports.set = set;
+exports.Memory = Memory;
---
+++ Binary/bitwise_operators.md
@@ -0,0 +1,3 @@
+# Resources:
+
+- [Math.floor VS Math.trunc JavaScript](http://stackoverflow.com/questions/38702724/math-floor-vs-math-trunc-javascript)
